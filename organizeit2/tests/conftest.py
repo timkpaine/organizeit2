657d373afe0747ab1e977fb562462a81b1c4c4ee
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2025-04-14 00:17:57 -0400

    Add limit/leaves/by to match/rematch queries

--- organizeit2/tests/conftest.py
+++ organizeit2/tests/conftest.py
@@ -1,3 +1,4 @@
+import os
 from tempfile import TemporaryDirectory
 
 from pytest import fixture
@@ -11,4 +12,14 @@ def tempdir():
 
 @fixture(scope="module", autouse=True)
 def directory_str():
+    os.system("touch organizeit2/tests/directory/subdir1/file1.png")
+    os.system("touch organizeit2/tests/directory/subdir1/file2.png")
+    os.system("touch organizeit2/tests/directory/subdir1/subsubdir1")
+    os.system("touch organizeit2/tests/directory/subdir1/subsubdir2")
+    os.system("touch organizeit2/tests/directory/subdir1/file1")
+    os.system("touch organizeit2/tests/directory/subdir1/file1.md")
+    os.system("touch organizeit2/tests/directory/subdir1/file1.txt")
+    os.system("touch organizeit2/tests/directory/subdir1/file2")
+    os.system("touch organizeit2/tests/directory/subdir1/file2.md")
+    os.system("touch organizeit2/tests/directory/subdir1/file2.txt")
     return "file://organizeit2/tests/directory"
